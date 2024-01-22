118e3beab8e478937903502d5e40d68d843d1853
YASHBOMBLE <yashbomble2002@gmail.com>
2024-01-22 17:20:56 +0530

    feat:home page nav ||  footer

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,10 +5,29 @@ import Footer from '../../component/Footer/Footer'
 function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
-      <div>
-        
+      <div className='row'>
+        <div className='col-4'>
+
+        </div>
+        <div className='col-4 '>
+
+          <span>
+            Name :
+          </span>
+          <input type='text' />
+          <span>
+            MO.NO :
+          </span>
+          <input type='text' />
+          <button>
+            Save
+          </button>
+        </div>
+        <div className='col-4'>
+
+        </div>
       </div>
       <Footer />
     </div>
