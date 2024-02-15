1283eecc31293d68e03e601ed55f3345cc6fab40
YASHBOMBLE <yashbomble2002@gmail.com>
2024-02-15 16:00:35 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -189,6 +189,11 @@ app.post('/updateContact',async (req,res)=>{
     //       }
     //})
 })
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+  });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
